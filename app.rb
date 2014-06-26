25d4bc7831d1afb31085517ddf5ebb0fde077099
alexshook <alexandra.shook@gmail.com>
2014-06-26 16:33:23 -0400

    returning json to user from endpoint and from search form

--- app.rb
+++ app.rb
@@ -17,10 +17,18 @@ end
 get '/search' do
   @query = params[:query].gsub(' ', '+')
   return status 404 if @query.nil?
-  # @results = JSON.parse(DuckySearch::Scraper.new.scrape_duck_duck_go(@query))
   @results = DuckySearch::Scraper.new.scrape_duck_duck_go(@query).to_json
   # call JSON.parse() on @results = DuckySearch::Scraper.new.scrape_duck_duck_go(@query).to_json to display readable
-  binding.pry
 
+  binding.pry
   erb :index
+end
+
+get '/:query' do
+  content_type :json
+  query = params[:query]
+  search = DuckySearch::Scraper.new.scrape_duck_duck_go(query).to_json
+  # @results = search.to_json
+  # json({ status: "success", query: query })
+  # return status 404 if query.nil?
 end
