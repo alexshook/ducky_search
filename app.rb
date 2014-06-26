c221400059566b0e851f4a149175507f22fb7e40
alexshook <alexandra.shook@gmail.com>
2014-06-26 18:37:06 -0400

    working ducky search

--- app.rb
+++ app.rb
@@ -5,7 +5,6 @@ require 'capybara'
 require 'capybara/dsl'
 require 'capybara-webkit'
 require 'json'
-require 'pry'
 require 'httparty'
 
 require_relative 'ducky_search'
@@ -16,19 +15,19 @@ end
 
 get '/search' do
   @query = params[:query].gsub(' ', '+')
-  return status 404 if @query.nil?
-  @results = DuckySearch::Scraper.new.scrape_duck_duck_go(@query).to_json
-  # call JSON.parse() on @results = DuckySearch::Scraper.new.scrape_duck_duck_go(@query).to_json to display readable
-
-  binding.pry
-  erb :index
+  @selection = params[:selection]
+  if @selection == 'summary'
+    @summary_results = DuckySearch::Scraper.new.get_topic_summary(@query)
+  else
+    return status 404 if @query.nil?
+    @results = DuckySearch::Scraper.new.scrape_duck_duck_go(@query)
+    binding.pry
+  end
+    erb :index
 end
 
 get '/:query' do
   content_type :json
   query = params[:query]
   search = DuckySearch::Scraper.new.scrape_duck_duck_go(query).to_json
-  # @results = search.to_json
-  # json({ status: "success", query: query })
-  # return status 404 if query.nil?
 end
--- ducky_search.rb
+++ ducky_search.rb
@@ -1,8 +1,7 @@
-Capybara.current_driver = :webkit
-# Capybara.javascript_driver = :webkit
-Capybara.app_host = "https://duckduckgo.com/"
-
 module DuckySearch
+  Capybara.current_driver = :webkit
+  Capybara.app_host = "https://duckduckgo.com/"
+
   class Scraper
     include Capybara::DSL
     def scrape_duck_duck_go(query_string)
@@ -12,9 +11,7 @@ module DuckySearch
       click_link_or_button 'S'
       # starting return results as JSON
       results_array = []
-      results = {}
-      # results_length = all('div.result__body').size
-      results_length = 20
+      limit_results = 20
       result_number = 0
       # get all results by div class, return title and snippet
       all('div.result__body').each do |result|
@@ -23,12 +20,17 @@ module DuckySearch
           title: result.find('h2.result__title').text,
           content: result.find('div.result__snippet').text
         }
-        # new_obj[result.find('h2.result__title').text] = result.find('div.result__snippet').text
         results_array << new_obj
         result_number += 1
-        break if result_number == results_length
+        break if result_number == limit_results
       end
       return results_array
+    end
+
+    def get_topic_summary(query_string)
+      results = HTTParty.get("http://api.duckduckgo.com/?q=#{query_string}&format=json")
+      abstract = JSON.parse(results)['AbstractText']
+      return abstract
     end
   end
 
